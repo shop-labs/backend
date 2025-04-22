b0a08c283412299038ec62654d3eadbd8a71b9f3
tracepanic <patrickscript@proton.me>
2025-04-22 19:01:33 +0300

    Add a simple user model

---
+++ prisma/migrations/20250422155924_/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT,
+    "password" TEXT NOT NULL,
+    "phone" TEXT,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "phoneVerified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
